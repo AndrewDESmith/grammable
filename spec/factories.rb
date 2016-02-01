4c402d583519e1d3dd57e28a7211739a298dfca8
AndrewDESmith <andrew.d.e.smith@gmail.com>
2016-02-01 23:16:12 +0000

    Set up tests for gram show action (found vs. not found)

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,4 +6,10 @@ FactoryGirl.define do
     password "secretPassword"
     password_confirmation "secretPassword"
   end
+
+  factory :gram do
+    message "hello"
+    # The association line means that the gram factory will automatically connect the user_id of this model (:gram) to a user created from a :user factory.
+    association :user
+  end
 end
